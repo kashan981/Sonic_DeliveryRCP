c1fe52706d198e1b237d060a73b9952413337499
kashan981 <kashanasad0@gmail.com>
2022-10-03 05:26:28 -0700

    first commit

---
+++ Sonic_Delivery_RCP/Properties.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonic_Delivery_RCP
+{
+    enum PropertyType
+    {
+        Id,
+        Name,
+        LinkText,
+        CssName,
+        ClassName
+    }
+    class Properties
+    {
+        public static IWebDriver Driver { get; set; }
+    }
+}
